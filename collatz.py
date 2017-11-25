76610db73d6c524c14547ab66c16abc12cc62aa4
khushmeet <khushmeetsingh1996@gmail.com>
2017-11-25 15:57:26 +0530

    efficient collatz conjecture with plotting

--- collatz.py
+++ collatz.py
@@ -1,22 +1,38 @@
 # naive implementation of collatz conjecture
+import matplotlib.pyplot as plt
+import numpy as np
+import time
 
 # Recursion problem
 def collatz_conjecture(number):
     """
     Collatz Conjecture
-    number is even -> number/2
-    number is odd -> 3number+1
+    n = number
+    number is even -> n/2
+    number is odd -> 3n+1
     """
-    if number == 1:
-        print(number)
-        print('On repeat from value 4, therefore returning!')
-        return
-    if number % 2 == 0:
-        print(number)
-        collatz_conjecture(number/2)
-    else:
-        print(number)
-        collatz_conjecture((3*number)+1)
+    travelled_nums = np.array([number], dtype=np.int64)
+    steps = 0
+    start = time.time()
+    while True:
+        if number % 2 == 0:
+            print(number)
+            number //= 2
+            travelled_nums = np.append(travelled_nums, int(number))
+            steps += 1
+            if number == 1:
+                break
+        if number % 2 != 0:
+            print(number)
+            number = (3 * number) + 1
+            travelled_nums = np.append(travelled_nums, int(number))
+            steps += 1
+    end = time.time()
+    print(f'Total steps taken: {steps}')
+    print(f'Total time: {end-start}s')
+    plt.plot(travelled_nums)
+    plt.xlabel('Numbers travelled')
+    plt.show()
 
-num = input('Enter number for collatz conjecture ')
+num = input('Enter number for collatz conjecture: ')
 collatz_conjecture(int(num))
